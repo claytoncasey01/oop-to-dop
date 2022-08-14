38267d26d6a90427cf1160811fe9ca47ec985217
Casey Clayton <claytoncasey01@gmail.com>
2022-08-13 23:57:22 -0400

    Quick code in main to test with

--- main.go
+++ main.go
@@ -4,18 +4,49 @@ import (
 	"fmt"
 	"github.com/claytoncasey01/go-oop-to-dod/dod"
 	"github.com/claytoncasey01/go-oop-to-dod/oop"
+	"strconv"
 )
 
 func main() {
-	authors := oop.MakeAuthors(1)
-	posts := oop.MakePosts(10, authors)
-	found := oop.FindPostsByAuthorName("Author 0", posts)
-	fmt.Printf("Length of Oop Posts: %d\n", len(posts))
-	fmt.Printf("Found %d posts\n", len(found))
+	authors := oop.MakeAuthors(100)
+	posts := oop.MakePosts(10000, authors)
 
-	dodAuthors := dod.MakeAuthors(1)
-	dodPosts := dod.MakePosts(10, dodAuthors)
-	fmt.Printf("Length of Dod Posts: %d\n", len(dodPosts.Ids))
+	dodAuthors := dod.MakeAuthors(100)
+	dodPosts := dod.MakePosts(10000, dodAuthors)
+
+	var command string
+	fmt.Print("Enter a command to run: ")
+	_, err := fmt.Scanln(&command)
+	fmt.Print("\n Enter the amount of times: ")
+	var n string
+	_, err = fmt.Scanln(&n)
+	parsedN, _ := strconv.Atoi(n)
+
+	if err == nil {
+		fmt.Printf("Running command %s %d times\n", command, parsedN)
+		switch command {
+		case "OopFindById":
+			for i := 0; i < parsedN; i++ {
+				oop.FindPostById(posts[0].Id, posts)
+			}
+		case "DodFindById":
+			for i := 0; i < parsedN; i++ {
+				dodPosts.FindById(dodPosts.Ids[50])
+				//fmt.Printf("Post Index: %d\n", postId)
+			}
+		case "OopFindByTitle":
+			for i := 0; i < parsedN; i++ {
+				oop.FindPostByTitle(posts[0].Title, posts)
+			}
+		case "OopFindByAuthorName":
+			for i := 0; i < parsedN; i++ {
+				oop.FindPostsByAuthorName("Author 0", posts)
+			}
+		default:
+			fmt.Println("Unknown argument")
+		}
+	}
+
 	//dodSurveys := dod.MakeSurveys(10, 0)
 	//dodSurveyInstances := dod.MakeSurveyInstances(1000, dodSurveys.IDs)
 	//fmt.Printf("DodSurveys Length: %d\n", len(dodSurveyInstances.IDs))
