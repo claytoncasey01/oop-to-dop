e30fb9145d38de89914659b757dbee8ceff5bd6d
Casey Clayton <claytoncasey01@gmail.com>
2022-08-19 15:13:26 -0400

    Fix benchmarks

--- rust/benches/oop_post_benchmarks.rs
+++ rust/benches/oop_post_benchmarks.rs
@@ -1,16 +1,24 @@
 use criterion::{BenchmarkId, black_box, Criterion, criterion_group, criterion_main};
 
 use rust_oop_to_dop::oop::post::{find_by_id, find_by_title, Post};
+use rust_oop_to_dop::oop::author::{Author};
 use rust_oop_to_dop::util::oop;
 
 fn new_post_bench(c: &mut Criterion) {
+    let post_title = String::from("Post title");
+    let post_body = String::from("Post body");
+    let author_name = String::from("Author 1");
+    let author_bio = String::from("bio for Author 1");
+    let author = Author::new( author_name, author_bio);
+
     c.bench_function("oop_new_post", |b| {
-        b.iter(|| Post::new(black_box(String::from("Post title")), black_box(String::from("Post Body"))));
+        b.iter(|| Post::new(black_box(post_title.clone()), black_box(post_body.clone()), black_box(&author)));
     });
 }
 
 fn find_by_id_bench(c: &mut Criterion) {
-    let posts = oop::create_posts(100);
+    let authors = oop::create_authors(50);
+    let posts = oop::create_posts(100, &authors);
     let post_id = posts[posts.len() / 2].id.clone();
 
     c.bench_function("oop_find_by_id", |b| {
