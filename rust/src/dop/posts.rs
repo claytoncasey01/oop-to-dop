4b812406fccf1ff63c5563347de55ad2e184de97
Casey Clayton <claytoncasey01@gmail.com>
2022-08-23 03:50:23 -0400

    Add update and delete for dod

--- rust/src/dop/posts.rs
+++ rust/src/dop/posts.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
+use crate::oop::post::find_post_by_id;
 
 pub struct Posts {
     pub ids: Vec<Uuid>,
@@ -8,6 +9,12 @@ pub struct Posts {
     pub published: Vec<bool>,
     pub updated_ats: Vec<DateTime<Utc>>,
     pub author_idxs: Vec<usize>,
+}
+
+pub struct PostUpdate {
+    pub id: Uuid,
+    pub title: Option<String>,
+    pub body: Option<String>,
 }
 
 impl Posts {
@@ -25,11 +32,47 @@ impl Posts {
         titles.into_iter().position(|found_title| *found_title == title)
     }
 
+    // TODO: Probably don't need the entire self struct, really only need ids, titles, updated_ats,
+    // TODO: and bodies. Check performance difference when benchmarking.
+    pub fn update(&mut self, updated_post: &PostUpdate) {
+        let post_idx = Self::find_by_id(updated_post.id, &self.ids);
+
+        match post_idx {
+            Some(idx) => {
+                // Handle each case to make sure we have a value
+                if let Some(updated_title) = &updated_post.title {
+                    self.titles[idx] = updated_title.to_string();
+                }
+
+                if let Some(updated_body) = &updated_post.body {
+                    self.bodies[idx] = updated_body.to_string();
+                }
+                self.updated_ats[idx] = Utc::now();
+            },
+            None => return
+        }
+    }
+
     pub fn publish(id: Uuid, ids: &Vec<Uuid>, published: &mut Vec<bool>) {
         let found_idx = Self::find_by_id(id, ids);
-        match found_idx {
-            Some(idx) => published[idx] = true,
-            None => {}
+        if let Some(idx) = found_idx {
+            published[idx] = true;
+        }
+    }
+
+    pub fn delete(&mut self, id: Uuid) {
+        // If we found a post with the id, remove all the data for it.
+        // TODO: Test this swap_remove might be fine here. Remove is o(n) worst case because
+        // TODO: We have to shift all elements to preserve order. However we might not care due to
+        // TODO: swap_remove replacing the removed with the last element meaning we should still
+        // TODO: be fine.
+        if let Some(idx) = Self::find_by_id(id, &self.ids) {
+            self.ids.remove(idx);
+            self.titles.remove(idx);
+            self.bodies.remove(idx);
+            self.updated_ats.remove(idx);
+            self.published.remove(idx);
+            self.author_idxs.remove(idx);
         }
     }
 }
